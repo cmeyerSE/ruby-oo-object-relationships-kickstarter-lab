6f96806ade7c12255298017818e55b5450788432
Cory <cameyer182@gmail.com>
2021-10-30 20:28:59 -0400

    completed assignment

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+    attr_accessor :title
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        backed = ProjectBacker.all.select{|projects| projects.project == self}
+        backed.map{|projects| projects.backer}
+    end
+end
